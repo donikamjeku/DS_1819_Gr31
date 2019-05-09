949ddef8b3d7625c5b84763644a5f6649f6a3e5f
Vesa Qilerxhiu <44980967+vesaqilerxhiu@users.noreply.github.com>
2019-05-09 14:28:12 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,6 +11,8 @@ using System.Security;
 using System.Security.Cryptography;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
+using System.IO;
+
 namespace Projekti1
 {
     public partial class Form1 : Form
@@ -22,12 +24,12 @@ namespace Projekti1
 
         private void btnEnkripto_Click(object sender, EventArgs e)
         {
-            
+            EncryptFile(txtPath.Text, txtPath1.Text);
         }
 
         private void btnDekripto_Click(object sender, EventArgs e)
         {
-
+            DecryptFile(txtPath.Text,txtPath1.Text);
         }
 
         private void btnOpen_Click(object sender, EventArgs e)
@@ -35,7 +37,7 @@ namespace Projekti1
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.ShowDialog();
             string pathi = ofd.FileName;
-            txtPath.AppendText(pathi);
+            txtPath.Text = pathi;
         }
 
         private void btnDestinacioni_Click(object sender, EventArgs e)
@@ -43,10 +45,76 @@ namespace Projekti1
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.ShowDialog();
             string pathi = ofd.FileName;
-            txtPath1.AppendText(pathi);
+            txtPath1.Text = pathi;
         }
+
+        private void EncryptFile(string inputFile, string outputFile)
+        {
+
+            try
+            {
+                string password = @"myKey123"; // Your Key Here
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                string cryptFile = outputFile;
+                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateEncryptor(key, key),
+                    CryptoStreamMode.Write);
+
+                FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+                int data;
+                while ((data = fsIn.ReadByte()) != -1)
+                    cs.WriteByte((byte)data);
+
+
+                MessageBox.Show("Enkriptimi u krye me sukses!");
+                fsIn.Close();
+                cs.Close();
+                fsCrypt.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Encryption failed!", "Error");
+            }
+        }
+        private void DecryptFile(string inputFile, string outputFile)
+        {
+
+            {
+                string password = @"myKey123"; // Your Key Here
+
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateDecryptor(key, key),
+                    CryptoStreamMode.Read);
+
+                FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+                int data;
+                while ((data = cs.ReadByte()) != -1)
+                    fsOut.WriteByte((byte)data);
+
+                MessageBox.Show("Dekriptimi u krye me sukses!");
+                fsOut.Close();
+                cs.Close();
+                fsCrypt.Close();
+
+            }
+        }
+
+
+
     }
-
-
-
 }
